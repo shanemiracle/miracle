72cb96197fd0f0e5a482a06c1fa20aabb3fad997
shanemiracle <824116664@qq.com>
2016-06-29 11:32:49 +0800

    2

--- application/index/controller/Car.php
+++ application/index/controller/Car.php
@@ -10,6 +10,7 @@ namespace app\index\controller;
 
 
 use think\controller\Rest;
+use think\Db;
 
 class Car extends Rest
 {
@@ -20,24 +21,42 @@ class Car extends Rest
     public function status() {
         $carNo = input('get.carSeqNo');
         $seatStatus = '';
-
         if($carNo != null) {
-            $ret = 1;
+            $carInfo = Db::table('car')->where('carNo',$carNo)->select();
 
-            for ($i=0;$i<34;$i++) {
-                if( $i%3==0&&$i%2==0) {
-                    $seatStatus = $seatStatus.($i+1)."|1,";
+            if( $carInfo ) {
+                for ($i=0;$i<34;$i++) {
+                    $seatStatus = $seatStatus.($i+1)."|".$carInfo['seatStatus'][$i].",";
                 }
-                else {
-                    $seatStatus = $seatStatus.($i+1)."|0,";
-                }
-
+                $seatStatus = $seatStatus."35|".$carInfo['seatStatus'][34];
+                $ret = 1;
             }
-            $seatStatus = $seatStatus."35|0";
+            else {
+                $ret = 2;
+            }
         }
         else {
             $ret = 2;
         }
+
+
+//        if($carNo != null) {
+//            $ret = 1;
+//
+//            for ($i=0;$i<34;$i++) {
+//                if( $i%3==0&&$i%2==0) {
+//                    $seatStatus = $seatStatus.($i+1)."|1,";
+//                }
+//                else {
+//                    $seatStatus = $seatStatus.($i+1)."|0,";
+//                }
+//
+//            }
+//            $seatStatus = $seatStatus."35|0";
+//        }
+//        else {
+//            $ret = 2;
+//        }
 
         return $this->response(['carSeqNo'=>$carNo,'retCode'=>$ret,'seatStatus'=>$seatStatus]);
     }
