ab7487688bbde8f24732115be05b17ca4bbb99eb
shanemiracle <824116664@qq.com>
2016-06-29 09:46:06 +0800

    2

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -156,6 +156,7 @@ html{-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}body{line-height:1.
         $price = $seatNum*3;
         $codeUrl="http://www.xjmiracle.com/pay?startPos=$startPos&endPos=$endPos&onTime=$time&seatNum=$seatNum&orderNo=$orderNo&price=$price";
 //        $codeUrl="http://192.9.60.133:8080/soyea_busdemo/view/pay.html?startPos=$startPos&endPos=$endPos&onTime=$time&seatNum=$seatNum&orderNo=$orderNo&price=$price";
+        $codeUrl = urlencode($codeUrl);
 
 
         if ($retCode==1) {
