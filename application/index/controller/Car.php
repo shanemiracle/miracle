709c1dd3f03139a538d41b67f72cc124f8f4904b
shanemiracle <824116664@qq.com>
2016-06-29 16:39:49 +0800

    1

--- application/index/controller/Car.php
+++ application/index/controller/Car.php
@@ -14,7 +14,23 @@ use think\Db;
 
 class Car extends Rest
 {
-    public $timeDay = '2016-';
+    public $timeDay = '2016-06-29';
+
+    /**
+     * @return string
+     */
+    public function getTimeDay()
+    {
+        return $this->timeDay;
+    }
+
+    /**
+     * @param string $timeDay
+     */
+    public function setTimeDay($timeDay)
+    {
+        $this->timeDay = $timeDay;
+    }
 
     public function index() {
 
@@ -29,23 +45,31 @@ class Car extends Rest
 
             if( $carInfo ) {
 
-//                $timeNow = date();
+                $timeNow = date('Y-M-j');
 //                $timeEnd = $carInfo[0]['timeend'];
 //                $timeNow = date('H:i:s');
 //                print $timeNow;
 
-//                if($timeNow>$timeEnd) {
-//                   if(1== Db::table('car')->where('carNo',$carNo)->update(['seatstatus'=>'00000000000000000000000000000000000000000000000000000000000000000000000000000000']) ) {
-//                       $carInfo = Db::table('car')->where('carNo',$carNo)->select();
-//                   }
-//                }
+                if($timeNow != getTimeDay()) {
 
-                for ($i=0;$i<34;$i++) {
-                    $seatStatus = $seatStatus.($i+1)."|".$statusSeat = $carInfo[0]['seatstatus'][$i].",";
+                   if(1== Db::table('car')->where('carNo',$carNo)->update(['seatstatus'=>'00000000000000000000000000000000000000000000000000000000000000000000000000000000']) ) {
+                       $carInfo = Db::table('car')->where('carNo',$carNo)->select();
+                       $this->setTimeDay($timeNow);
+                   }
+                    else {
+                        $ret = 2;
+                    }
                 }
-                $statusSeat = $carInfo[0]['seatstatus'][34];
-                $seatStatus = $seatStatus."35|".$statusSeat;
-                $ret = 1;
+
+                if ( $ret != 2 ) {
+                    for ($i=0;$i<34;$i++) {
+                        $seatStatus = $seatStatus.($i+1)."|".$statusSeat = $carInfo[0]['seatstatus'][$i].",";
+                    }
+                    $statusSeat = $carInfo[0]['seatstatus'][34];
+                    $seatStatus = $seatStatus."35|".$statusSeat;
+                    $ret = 1;
+                }
+
             }
             else {
                 $ret = 2;
