b78b1d68ef9209cb8670ff1ca0b9130a67c0616b
shanemiracle <824116664@qq.com>
2016-07-18 14:25:50 +0800

    1

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -39,6 +39,6 @@ class Index extends Rest
     }
 
     public function up() {
-        return '< action="index/User/logoUpload" enctype="multipart/form-data" method="post"> <input type="file" name="image"/> <br> <input type="submit" value="上传"/> </form> ';
+        return '<form action="index/User/logoUpload" enctype="multipart/form-data" method="post"> <input type="file" name="image"/> <br> <input type="submit" value="上传"/> </form> ';
     }
 }
