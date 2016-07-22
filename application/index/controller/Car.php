83d3b709e3f7a7a17ec8558b1db0a3ee2149ef68
shanemiracle <824116664@qq.com>
2016-07-22 15:29:10 +0800

    1

--- application/index/controller/Car.php
+++ application/index/controller/Car.php
@@ -429,14 +429,14 @@ class Car extends Rest
         $retData = array();
         $retNum = 0;
 
-
+        $weekarray = array("日", "一", "二", "三", "四", "五", "六");
+        $weekNum = date('w');
 
         for ($day = 0; $day < 5; $day++)
         {
-            $weekarray = array("日", "一", "二", "三", "四", "五", "六");
-            date_default_timezone_set('UTC');
-            $week = "星期".date("w");
-
+            $week = "星期".$weekarray[$weekNum];
+            $weekNum++;
+            
             $tableSchedule = new tableSchedule();
             if (0 != $tableSchedule->findByCarTime($carno, $onTime)) {
                 $this->setDesc("carno $carno 在 $onTime 时间点没有车次");
