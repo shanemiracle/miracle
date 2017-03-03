7b4b46c0d68ff9add5dd56b9630a3e1543807e65
shanemiracle <824116664@qq.com>
2017-03-03 15:18:01 +0800

    1

--- application/database.php
+++ application/database.php
@@ -14,7 +14,7 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => 'localhost',
+    'hostname'       => '115.236.177.85',
     // 数据库名
     'database'       => 'bus',
     // 用户名
