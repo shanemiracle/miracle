cf20dd5bed11e0b42f3b6f259e61d164573978a5
shanemiracle <824116664@qq.com>
2019-04-28 11:14:45 +0800

    Update database.php

--- application/database.php
+++ application/database.php
@@ -14,13 +14,13 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => '115.236.177.85',
+    'hostname'       => '127.0.0.1',
     // 数据库名
     'database'       => 'bus',
     // 用户名
     'username'       => 'admin',
     // 密码
-    'password'       => '2ws4rf6yh',
+    'password'       => '123456',
     // 端口
     'hostport'       => '3306',
     // 连接dsn
